0b5dd13e585808592cf058b82a75b5d97440cea8
mathesond2 <mathesond2@gmail.com>
2023-02-11 15:14:16 -0500

    add layout structure, nav

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import { Container, Flex, Input, InputGroup, InputLeftElement, Link } from '@chakra-ui/react';
+import { useState } from 'react';
+
+export default function Navbar() {
+  const [search, setSearch] = useState('');
+  return (
+    <Container maxW="full" as="header" borderBottomWidth="1px" paddingX={10} paddingY={6}>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Link href="/" _hover={{ textDecoration: 'none' }} fontWeight="extrabold">
+          Asset Overview
+        </Link>
+        <InputGroup width={300}>
+          <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
+          <Input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+        </InputGroup>
+      </Flex>
+    </Container>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@/util/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
