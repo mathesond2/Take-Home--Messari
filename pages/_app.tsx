ff84c43a2240dcb624291257378a71a2444d7dfc
mathesond2 <mathesond2@gmail.com>
2023-02-12 18:35:12 -0500

    move head content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,23 @@ import { theme } from '@/util/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import Head from 'next/head';
+import HeadContent from '../components/HeadContent';
 import { AssetProvider } from '@/util/AssetContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AssetProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AssetProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <HeadContent />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <AssetProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AssetProvider>
+      </ChakraProvider>
+    </>
   );
 }
